9ac0e3f73c4f1da6ce2b3985875a99f5e4a7a63e
julien <julien.griffault90@gmail.com>
2019-01-10 10:31:18 +0100

    Création voiture

--- stevenProjet/src/main/java/cgi/stevenProjet/App.java
+++ stevenProjet/src/main/java/cgi/stevenProjet/App.java
@@ -1,5 +1,7 @@
 package cgi.stevenProjet;
 
+import cgi.stevenProjet.bean.Voiture;
+
 /**
  * Hello world!
  *
@@ -8,6 +10,13 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello Steven!" );
+        System.out.println( "App lauched\n------------------------\n" );
+        
+        Voiture voiture = new Voiture();
+        Voiture voiture2 = new Voiture(5, true);
+        
+        System.out.println(voiture.toString());
+        System.out.println(voiture2.toString());
+        
     }
 }
