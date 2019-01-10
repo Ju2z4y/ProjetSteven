3051f8895da046d2a73682e007fd1908ff601169
julien <julien.griffault90@gmail.com>
2019-01-10 17:26:42 +0100

    Solution

--- stevenProjet/src/main/java/cgi/stevenProjet/bean/Soupe.java
+++ stevenProjet/src/main/java/cgi/stevenProjet/bean/Soupe.java
@@ -1,13 +1,13 @@
 package cgi.stevenProjet.bean;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Soupe {
 	protected double poidsEpluchure;
 	protected ArrayList<Legume> listeLegume = new ArrayList<Legume>();
-	protected ArrayList<Tomate> listeTomate = new ArrayList<Tomate>();
-	protected ArrayList<Patate> listePatate = new ArrayList<Patate>();
-	protected ArrayList<Carotte> listeCarotte = new ArrayList<Carotte>();
+
 	
 	public Soupe() {
 		
@@ -25,62 +25,86 @@ public class Soupe {
 	public void setListeLegume(ArrayList<Legume> listeLegume) {
 		this.listeLegume = listeLegume;
 	}
-
-	public ArrayList<Tomate> getListeTomate() {
-		return listeTomate;
-	}
-	public void Tomate(ArrayList<Tomate> listeTomate) {
-		this.listeTomate = listeTomate;
-	}
-
-	public ArrayList<Patate> getListePatate() {
-		return listePatate;
-	}
-	public void Patate(ArrayList<Patate> listePatate) {
-		this.listePatate = listePatate;
-	}
-	
-	public ArrayList<Carotte> getListeCarotte() {
-		return listeCarotte;
-	}
-	public void Carotte(ArrayList<Carotte> listeCarotte) {
-		this.listeCarotte = listeCarotte;
-	}
 	
 	public void addLegume(Legume leg) {
 		this.listeLegume.add(leg);
 	}
 	
 	public String toString() {
-		double poidsTotalLegume = 0;
-		double poidsTotalTomate = 0;
-		double poidsTotalPatate = 0;
-		double poidsTotalCarotte = 0;
-		String resultat = "";
-		String resLeg = "";
-		String resLeg = "";
-		String resultat = "";
+		String resultat;
+		Map<String, Integer> tomates = new HashMap<String, Integer>();
+		Map<String, Integer> carottes = new HashMap<String, Integer>();
+		Map<String, Integer> patates = new HashMap<String, Integer>();
+		int nbTomate = 0;
+		double poidsTomate = 0;
+		int nbCarotte = 0;
+		double poidsCarotte = 0;
+		int nbPatate = 0;
+		double poidsPatate = 0;
+		
 		
 		for (Legume leg : listeLegume) {
-			poidsTotalLegume += leg.getPoids();
+			if (leg instanceof Tomate) {
+				nbTomate++;
+				poidsTomate += leg.getPoids();
+				if (tomates.get(leg.getVariete()) == null) {
+					tomates.put(leg.getVariete(), 1);
+				} else {
+					int incr = tomates.get(leg.getVariete());
+					incr ++;
+					tomates.replace(leg.getVariete(), incr);
+				}
+			}
+			if (leg instanceof Carotte) {
+				nbCarotte++;
+				poidsCarotte += leg.getPoids();
+				if (carottes.get(leg.getVariete()) == null) {
+					carottes.put(leg.getVariete(), 1);
+				} else {
+					int incr = carottes.get(leg.getVariete());
+					incr ++;
+					carottes.replace(leg.getVariete(), incr);
+				}
+			}
+			if (leg instanceof Patate) {
+				nbPatate++;
+				poidsPatate += leg.getPoids();
+				if (patates.get(leg.getVariete()) == null) {
+					patates.put(leg.getVariete(), 1);
+				} else {
+					int incr = patates.get(leg.getVariete());
+					incr ++;
+					patates.replace(leg.getVariete(), incr);
+				}
+			}
+		}
+		resultat = (poidsCarotte + poidsPatate + poidsTomate) + "g de soupe : \n" + 
+		nbTomate + " tomates pour " + poidsTomate + "g. Variétés = ";
+		
+		int incr2 = 0;
+		for (Map.Entry<String,Integer> e : tomates.entrySet()) {
+			resultat += (incr2==0?"":", ") + e.getKey() + " : " + e.getValue();
+			incr2++;
 		}
 		
-		for (Tomate tom : listeTomate) {
-			poidsTotalTomate += tom.getPoids();
-		}
-		for (Patate pat : listePatate) {
-			poidsTotalPatate += pat.getPoids();
-		}
-		for (Carotte car : listeCarotte) {
-			poidsTotalCarotte += car.getPoids();
+		resultat += "\n" + nbCarotte + " carotte pour " + poidsCarotte + "g. Variétés = ";
+		
+		incr2 = 0;
+		for (Map.Entry<String,Integer> e : carottes.entrySet()) {
+			resultat += (incr2==0?"":", ") + e.getKey() + " : " + e.getValue();
+			incr2++;
 		}
 		
+		resultat += "\n" + nbPatate + " patates pour " + poidsPatate + "g. Variétés = ";
 		
-//		resultat = "" +poidsTotalLegume + " g de soupe :" + "\n";
+		incr2 = 0;
+		for (Map.Entry<String,Integer> e : patates.entrySet()) {
+			resultat += (incr2==0?"":", ") + e.getKey() + " : " + e.getValue();
+			incr2++;
+		}
 		
-		resLeg = "" +poidsTotalLegume + " g de soupe :" + "\n";
+		resultat += "\nPoids des épluchures total pour la soupe : " + this.getPoidsEpluchure() +"g\n\n";
 		
-		resultat = ""+
 		return resultat;
 		
 	}
@@ -94,7 +118,7 @@ public class Soupe {
 //
 //	    500 g de soupe :
 //
-//	        3 pour 100g Tomates : 2 var1, 1var2
+//	        3 Tomates pour 100g  : 2 var1, 1var2
 //
 //	        4 pour 50g Carottes : 1 var1, 3 var2
 //
